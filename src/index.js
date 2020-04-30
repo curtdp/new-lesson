5c152923e1a0ba5c9a768e05c9c347f7ae40a5c7
Igor Oliinyk <igor.oleynikdp@gmail.com>
2020-04-30 21:51:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>Hello Vanilla JavaScript!</h1>
 <div>
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
